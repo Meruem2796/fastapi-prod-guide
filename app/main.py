3ad23646b589fe1e44c7b6efc6e3f1336dd03d3c
Abdou_Mourid <abdoumouhid96@yahoo.com>
2026-08-01 01:23:22 +0200

    Create:Setup POST route

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
+from datetime import datetime
+
 import uvicorn
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+from pathlib import Path
 
 
 class Settings(BaseSettings):
     mongo_uri: str
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).resolve().parent.parent / ".env",
+        extra="ignore",
+    )
 
 
 settings = Settings()
@@ -15,6 +24,33 @@ db_client = AsyncIOMotorClient(settings.mongo_uri)
 db = db_client.todoDb
 
 app = FastAPI()
+
+
+class Todo(BaseModel):
+    title: str
+    completed: bool = False
+
+
+class TodoId(BaseModel):
+    id: str
+
+
+@app.post("/todos", response_model=TodoId)
+async def create_todo(payload: Todo) -> TodoId:
+    """
+    Create a new Todo
+    """
+    now = datetime.utcnow()
+    insert_result = await db.todos.insert_one(
+        {
+            "title": payload.title,
+            "completed": payload.completed,
+            "created_date": now,
+            "updated_date": now,
+        }
+    )
+
+    return TodoId(id=str(insert_result.inserted_id))
 
 
 if __name__ == "__main__":
