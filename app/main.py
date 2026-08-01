c4f7b309c60e877d9668fe0ede6054d660f7fec2
Abdou_Mourid <abdoumouhid96@yahoo.com>
2026-08-01 02:13:37 +0200

    Read:setup two GET routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
 import uvicorn
-from fastapi import FastAPI
+from bson import ObjectId
+from fastapi import FastAPI, HTTPException, Path
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -25,6 +26,8 @@ db = db_client.todoDb
 
 app = FastAPI()
 
+MONGO_ID_REGEX = r"^[a-f\d]{24}$"
+
 
 class Todo(BaseModel):
     title: str
@@ -33,6 +36,15 @@ class Todo(BaseModel):
 
 class TodoId(BaseModel):
     id: str
+
+
+class TodoRecord(TodoId, Todo):
+    created_date: datetime
+    updated_date: datetime
+
+
+class NotFoundException(BaseModel):
+    detail: str = "Not Found"
 
 
 @app.post("/todos", response_model=TodoId)
@@ -51,6 +63,52 @@ async def create_todo(payload: Todo) -> TodoId:
     )
 
     return TodoId(id=str(insert_result.inserted_id))
+
+
+@app.get(
+    "/todos/{id}",
+    response_model=TodoRecord,
+    responses={
+        404: {"description": "Not Found", "model": NotFoundException},
+    },
+)
+async def get_todo(
+    id: str = Path(description="Todo ID", pattern=MONGO_ID_REGEX)
+) -> TodoRecord:
+    """
+    Get a Todo
+    """
+    doc = await db.todos.find_one({"_id": ObjectId(id)})
+    if not doc:
+        raise HTTPException(status_code=404, detail="Not Found")
+
+    return TodoRecord(
+        id=str(doc["_id"]),
+        title=doc["title"],
+        completed=doc["completed"],
+        created_date=doc["created_date"],
+        updated_date=doc["updated_date"],
+    )
+
+
+@app.get("/todos", response_model=list[TodoRecord])
+async def get_todos() -> list[TodoRecord]:
+    """
+    Get Todos
+    """
+    todos: list[TodoRecord] = []
+    async for doc in db.todos.find():
+        todos.append(
+            TodoRecord(
+                id=str(doc["_id"]),
+                title=doc["title"],
+                completed=doc["completed"],
+                created_date=doc["created_date"],
+                updated_date=doc["updated_date"],
+            )
+        )
+
+    return todos
 
 
 if __name__ == "__main__":
